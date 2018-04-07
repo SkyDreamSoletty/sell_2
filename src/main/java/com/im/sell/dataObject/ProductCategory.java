9c20dce1ffa4708f4044c05fc5086cd57f939940
wyj <w861642669@gmail.com>
2018-04-08 01:45:17 +0800

    省略get/set方法 使用@Data注解替代

--- src/main/java/com/im/sell/dataObject/ProductCategory.java
+++ src/main/java/com/im/sell/dataObject/ProductCategory.java
@@ -1,5 +1,6 @@
 package com.im.sell.dataObject;
 
+import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
@@ -14,6 +15,7 @@ import java.util.Date;
 
 @Entity
 @DynamicUpdate
+@Data
 public class ProductCategory {
 
     /**
@@ -31,57 +33,12 @@ public class ProductCategory {
      */
     private Integer categoryType;
 
-    private Date createTime;
-
-    private Date updateTime;
-
-    public Integer getCategoryId() {
-        return categoryId;
+    public ProductCategory() {
     }
 
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
+    public ProductCategory(String categoryName, Integer categoryType) {
         this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
         this.categoryType = categoryType;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                '}';
     }
 }
 
