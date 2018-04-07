78b0f0143ff6397438f0896e660e042ad130dd8a
wyj <w861642669@gmail.com>
2018-04-08 01:46:10 +0800

    添加findByCategoryTypeIn方法的单元测试

--- src/test/java/com/im/sell/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/im/sell/repository/ProductCategoryRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.im.sell.repository;
 
 import com.im.sell.dataObject.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -25,15 +30,27 @@ public class ProductCategoryRepositoryTest {
     }
 
     @Test
+    @Transactional//完全回滚 不会在数据库添加测试数据
     public void saveTest(){
-        Optional<ProductCategory> tmp = repository.findById(1);
-        ProductCategory productCategory = tmp.get();
-        productCategory.setCategoryType(8);
+//        Optional<ProductCategory> tmp = repository.findById(1);
+//        ProductCategory productCategory = tmp.get();
+//        productCategory.setCategoryType(5);
 //        ProductCategory productCategory = new ProductCategory();
 //        productCategory.setCategoryId(1);
 //        productCategory.setCategoryName("女生最爱");
 //        productCategory.setCategoryType(2);
-        repository.save(productCategory);
+//        repository.save(productCategory);
+        ProductCategory productCategory = new ProductCategory("男生最爱",4);
+        ProductCategory result = repository.save(productCategory);
+        Assert.assertNotNull(result);
+        Assert.assertNotEquals(null,result);
+    }
+
+    @Test
+    public void findByCategoryTypeInTest(){
+        List<Integer> categoryTypeList = Arrays.asList(1,2,3,4,8);
+        List<ProductCategory> result = repository.findByCategoryTypeIn(categoryTypeList);
+        Assert.assertNotEquals(0,result.size());
     }
 
 }
